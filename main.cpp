a7c68091f63d103948e07a6381745b0f425d520c
nishiokozo <nishiokozo@gmail.com>
2019-10-07 01:24:42 +0900

    tire 剛体実験２

--- main.cpp
+++ main.cpp
@@ -2143,25 +2143,110 @@ struct Apr : public Sys
 			// 描画	タイヤ実験
 			//=================================
 			{
-				static vect3 pos = vect3(0,2.0,0);
-				static vect3 acc= vect3(0,0,0.0);
-				static float radius = 0.5;
-				static float rad = deg2rad(0);		//	角速度
-				static float head = deg2rad(35);	//	タイヤの方向
-				static float bank = deg2rad(0);	//	各速度
+				const vect3 G_pos = vect3(0,2.0,0);
+				const vect3 G_acc= vect3(0,0,0.0);
+				const float G_radius = 0.5;
+				const float G_head = deg2rad(35);	//	タイヤの方向
+				const float G_bank = deg2rad(40);	//	回転角
+				const float G_rspd = deg2rad(0);	//	角速度
+				const float G = 9.8;				// 重力加速度
+				const float grate = 9.8 / 60/60;	// 重力加速度
+
+				static vect3 pos = G_pos;
+				static vect3 acc1 = G_acc;
+				static vect3 acc2 = G_acc;
+				static float radius = G_radius;
+				static float head = G_head;
+				static float bank = G_bank;
+				static float rspd = G_rspd;
+
 				
-				if ( mouse.F.hi ) acc += vect3( sin(head), 0, cos(head) )*0.02;
-				if ( mouse.B.hi ) acc += vect3( sin(head), 0, cos(head) )*-0.01;
-				if ( mouse.R.hi ) bank += deg2rad(10);
+				// 動き
+				{
+					// 強制
+					{
+						// リセット
+						if ( keys.R.hi ) 
+						{
+							pos = G_pos;
+							acc1 = G_acc;
+							acc2 = G_acc;
+							radius = G_radius;
+							head = G_head;
+							bank = G_bank;
+							rspd = G_rspd;
+						}
+						if ( mouse.F.hi ) 
+						{
+							acc1 += vect3( sin(head), 0, cos(head) )*0.02;
+							acc2 += vect3( sin(head), 0, cos(head) )*0.02;
+						}
+						if ( mouse.B.hi ) bank += deg2rad(5);
+						if ( mouse.R.hi ) bank += deg2rad(-5);
+					}
 
-				acc.y += -0.01;
+					// 重力
+					{
+						acc1.y -= grate;
+						acc2.y -= grate;
+						pos += (acc1+acc2)/2 ;
+					}
 
-				pos += acc ;
+					// 回転
+					{
+						bank += rspd;
+					}
+				}
 
-				// 衝突
-				if ( radius > pos.y ) pos.y = radius;
-		
+				float high = abs(radius*cos(bank));
+				
+				// 衝突	地面
+				if ( high > pos.y ) 
+				{
 
+					pos.y = high;
+
+					// バウンド
+					if ( bank > 0 )
+					{
+					}
+					else
+					{
+					}
+//						acc.y = -acc.y * 0.5; 
+//					acc.y = 0;
+#if 0
+					// 回転
+					{
+						float a = atan( acc.y * sin(bank) / radius );
+						rspd = a;
+					}
+					// 衝突 bank
+					if ( bank > deg2rad(90) ) bank -= deg2rad(180);
+					if ( bank < deg2rad(-90) ) bank += deg2rad(180);
+#endif
+				}
+				
+				// 設置地面
+				if ( high == pos.y ) 
+				{
+					// 衝突 bank
+					if ( bank > deg2rad(90) ) bank -= deg2rad(180);
+					if ( bank < deg2rad(-90) ) bank += deg2rad(180);
+
+					// 回転
+					{
+						float a = atan( grate * sin(bank) / radius );
+						rspd += a;
+					}
+				}
+
+				// 減衰	空気抵抗
+				{
+						rspd *= 0.99;
+				}
+
+				// 描画
 				tire.DrawRing( gra, pers, pos, head, bank, radius );
 			}
 
