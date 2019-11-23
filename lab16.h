dc8c103620bcd09d0092ce0e6a0e712ff1b2d4f5
nishiokozo <nishiokozo@gmail.com>
2019-11-24 05:44:34 +0900

    lab16 pImplイディオムにより対応

--- lab16.h
+++ lab16.h
@@ -1,10 +1,10 @@
 struct Lab16 : LabObj
 {
 	Lab16();
-	~Lab16();
 
 	void Update( Lab& lab, SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, float delta, int& text_y, Cp& cp );
 
-    class Lab16Impl* pImpl; 
+    struct Lab16Impl;
+    std::unique_ptr<Lab16Impl> pImpl; 
 
 };
