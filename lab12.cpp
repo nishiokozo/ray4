a2b21ce05f050278568db769a3b14e4b46763afd
nishoikozo <nishiokozo@gmail.com>
2019-11-28 15:34:04 +0900

    lab12 同時衝突に計算修正

--- lab12.cpp
+++ lab12.cpp
@@ -50,8 +50,6 @@ struct Ball:Obj
 	float	fspin;
 
 	vect3	pn;
-	vect3	vn;
-	mat33	mn = midentity();
 	
 	Ball() : Obj(vect3(0,0,0)) {}
 
@@ -109,6 +107,9 @@ void Lab12::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, flo
 	// 初期化：パラメータ
 	if ( !m.bInitParam )
 	{
+#define NUM 3
+#if NUM==1
+	// b2 斜め45°
 		m.bInitParam = true;
 		{
 			b1.pos	= vect3( -2	, 1.0,  0.0 );
@@ -128,11 +129,81 @@ void Lab12::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, flo
 		}
 
 		// 転がり
+		if(0){
+			float ene = 0.1;
+			vect3 dir = vect3(1,0,0).normalize();
+			b1.vel += ene * dir;
+		}
+		if(1){
+			float ene = 0.1;
+			vect3 dir = vect3(-1,0,1).normalize();
+			b2.vel += ene * dir;
+		}
+#endif
+#if NUM==2
+	// b1 b2 斜め90°
+		m.bInitParam = true;
+		{
+			b1.pos	= vect3( -3	, 1.0, -3.0 );
+			b1.vel	= vect3(  0	, 0.0,  0.0 );
+			b1.radius = 1.0;
+			b1.mat	= midentity();
+			b1.vaxis	= vect3(0,0,1);
+			b1.fspin	= 0.0;
+		}
+		{
+			b2.pos	= vect3(  3	, 1.0, -3.0 );
+			b2.vel	= vect3(  0	, 0.0,  0.0 );
+			b2.radius = 1.0;
+			b2.mat	= midentity();
+			b2.vaxis	= vect3(0,0,1);
+			b2.fspin	= 0.0;
+		}
+
+		// 転がり
+		{
+			float ene = 0.1;
+			vect3 dir = vect3(1,0,1).normalize();
+			b1.vel += ene * dir;
+		}
 		{
 			float ene = 0.1;
 			vect3 dir = vect3(-1,0,1).normalize();
 			b2.vel += ene * dir;
 		}
+#endif
+#if NUM==3
+	// b1 b2 正面衝突°
+		m.bInitParam = true;
+		{
+			b1.pos	= vect3( -3	, 1.0,  0.0 );
+			b1.vel	= vect3(  0	, 0.0,  0.0 );
+			b1.radius = 1.0;
+			b1.mat	= midentity();
+			b1.vaxis	= vect3(0,0,1);
+			b1.fspin	= 0.0;
+		}
+		{
+			b2.pos	= vect3(  3	, 1.0,  0.0 );
+			b2.vel	= vect3(  0	, 0.0,  0.0 );
+			b2.radius = 1.0;
+			b2.mat	= midentity();
+			b2.vaxis	= vect3(0,0,1);
+			b2.fspin	= 0.0;
+		}
+
+		// 転がり
+		{
+			float ene = 0.1;
+			vect3 dir = vect3(1,0,0).normalize();
+			b1.vel += ene * dir;
+		}
+		{
+			float ene = 0.1;
+			vect3 dir = vect3(-1,0,0).normalize();
+			b2.vel += ene * dir;
+		}
+#endif
 
 	}
 
@@ -165,10 +236,14 @@ void Lab12::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, flo
 			b2.pn = b2.pos;
 
 			vect3	N = (b1.pos-b2.pos).normalize();
-			vect3	vel = func_reflect( b2.vel, N, 1.0 );
+			vect3	vel1 = func_reflect( b1.vel, N, 1.0 );
+			vect3	vel2 = func_reflect( b2.vel, N, 1.0 );
 
-			b1.vel = dot(-vel,N)*N;
-			b2.vel = vel + b1.vel;
+			vect3 v1 = dot(-vel2,N)*N;
+			vect3 v2 = dot(-vel1,N)*N;
+
+			b1.vel += v1 - v2;
+			b2.vel += v2 - v1;
 		}
 	}
 	
