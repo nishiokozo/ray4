7c0df8aea774460169a93e6301fa4875d2395cdf
nishiokozo <nishiokozo@gmail.com>
2019-11-30 15:58:05 +0900

    lab9 Plate追加

--- lab9.cpp
+++ lab9.cpp
@@ -46,11 +46,13 @@ struct Ball9:Obj
 	float	radius = 0.1;
 	mat33	mat = midentity();
 	bool	flgOn = false;	// 接地フラグ
+	vect3	Q;
 	vect3	vaxis;
 	float	fspin;
 
 	vect3	pn;
 	vect3	vn;
+	mat33	mn = midentity();
 
 	Ball9() : Obj(vect3(0,0,0)) {}
 
@@ -69,7 +71,7 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 
 	//画面クリア
 	gra.Clr(rgb(0.3,0.3,0.3));
-	pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), midentity(), 16, 16, 1, rgb(0.2,0.2,0.2) );
+//	pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), midentity(), 16, 16, 1, rgb(0.2,0.2,0.2) );
 	gra.Print(1,(float)text_y++,string("9 : Ball & Ball & Gravity")); 
 
 	if ( !m.bInitAll )
@@ -77,12 +79,14 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 		m.bInitAll = true;
 
 		// カメラ
-		pers.cam.pos = vect3(	0.0,	1.0, -20.0 );
+		pers.cam.pos = vect3(	0.0,	10.0, -30.0 );
 		pers.cam.at = vect3( 	0.0,	1.0, 0.0 );
 
 		//点
 		for ( Obj* p : m.tbl_pObj ) delete p;
 		m.tbl_pObj.clear();
+		m.tbl_pObj.emplace_back( new Obj(vect3( 0.05	, 0.0,	0.01 )) );	// 平面原点
+		m.tbl_pObj.emplace_back( new Obj(vect3( 0.0		, 0.5,  0.0 )) );	// 平面法線
 		m.tbl_pObj.emplace_back( new Ball9 );
 		m.tbl_pObj.emplace_back( new Ball9 );
 
@@ -91,8 +95,12 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 
 	}
 
-	Ball9&	b1 = *dynamic_cast<Ball9*>(m.tbl_pObj[0]);
-	Ball9&	b2 = *dynamic_cast<Ball9*>(m.tbl_pObj[1]);
+	// 設定値
+	vect3	vg	= vect3(0,G,0);		// 重力加速度ベクトル
+	vect3	plate_p	= m.tbl_pObj[0]->pos;
+	vect3	plate_n	= (m.tbl_pObj[1]->pos-plate_p).normalize();
+	Ball9&	b1 = *dynamic_cast<Ball9*>(m.tbl_pObj[2]);
+	Ball9&	b2 = *dynamic_cast<Ball9*>(m.tbl_pObj[3]);
 
 	// 初期化：パラメータ
 	if ( !m.bInitParam )
@@ -100,7 +108,7 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 	// b1 b2 正面衝突°
 		m.bInitParam = true;
 		{
-			b1.pos		= vect3(  0	, 4.0,  0.0 );
+			b1.pos		= vect3( -3	, 6.0,  0.0 );
 			b1.vel		= vect3(  0	, 0.0,  0.0 );
 			b1.radius 	= 1.0;
 			b1.mat		= midentity();
@@ -108,7 +116,7 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 			b1.fspin	= 0.0;
 		}
 		{
-			b2.pos		= vect3(  1	, 1.0,  0.0 );
+			b2.pos		= vect3(  2	, 4.0,  0.0 );
 			b2.vel		= vect3(  0	, 0.0,  0.0 );
 			b2.radius 	= 1.0;
 			b2.mat		= midentity();
@@ -118,13 +126,13 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 
 		// 転がり
 		{
-			float ene = 0;
+			float ene = 7;
 			vect3 dir = vect3(1,0,0).normalize();
 			b1.vel += ene * dir;
 		}
 		{
-			float ene = 0;
-			vect3 dir = vect3(-1,0,0).normalize();
+			float ene = 1;
+			vect3 dir = vect3(1,0,0).normalize();
 			b2.vel += ene * dir;
 		}
 
@@ -144,14 +152,9 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 	if ( keys.S.hi )
 	{
 		float ene = 6;
-		vect3 dir = vect3(-1,0,0);
+		vect3 dir = vect3(1,0,0);
 		b2.vel += ene * dir;
 	}
-
-	// 定数
-	vect3	vg	= vect3(0,G,0);		// 重力加速度ベクトル
-	vect3	plate_p	= vect3(0,0,0);
-	vect3	plate_n	= vect3(0,1,0);
 
 	// 移動
 	b1.vn = b1.vel + vg * delta;
@@ -175,25 +178,30 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 		ball.fspin = th;
 	}; 
 
+	auto funcBound = [&]( Ball9& ball, vect3& N )
+	{
+		vect3 d = ball.vn * delta;
+		ball.pn -= dot( d, N ) * N;
+		ball.vn = func_reflect( ball.vn, N, (1.0-pow(1-0.3,2)) );
+		funcSpin(ball, N);
+	};
 	// 床との衝突
 	{
 		auto[flg,q0,t] = func_intersect_Plate_SegCurve_ball( plate_p, plate_n, b1.pos, b1.radius, -0.0001, delta, dot(vg,plate_n)*plate_n, dot(b1.vn,plate_n)*plate_n );
 		b1.flgOn = flg;
+		b1.Q = q0;
 		if ( b1.flgOn )
 		{
-			b1.pn.y =  b1.pos.y;
-			b1.vn.y = -b1.vel.y * (1.0-pow(1-0.3,2));//0.3
-			funcSpin(b1, plate_n);
+			funcBound( b1, plate_n );
 		}
 	}
 	{
 		auto[flg,q0,t] = func_intersect_Plate_SegCurve_ball( plate_p, plate_n, b2.pos, b2.radius, -0.0001, delta, dot(vg,plate_n)*plate_n, dot(b2.vn,plate_n)*plate_n );
 		b2.flgOn = flg;
+		b2.Q = q0;
 		if ( b2.flgOn )
 		{
-			b2.pn.y =  b2.pos.y;
-			b2.vn.y = -b2.vel.y * (1.0-pow(1-0.3,2));
-			funcSpin(b2, plate_n);
+			funcBound( b2, plate_n );
 		}
 	}
 
@@ -203,11 +211,12 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 		{
 
 			vect3	N = (b1.pos-b2.pos).normalize();
-			vect3	vel1 = func_reflect( b1.vn, N, 1.0 );
-			vect3	vel2 = func_reflect( b2.vn, N, 1.0 );
 
 			funcSpin(b1, N);
 			funcSpin(b2, N);
+
+			vect3	vel1 = func_reflect( b1.vn, N, 1.0 );
+			vect3	vel2 = func_reflect( b2.vn, N, 1.0 );
 
 			vect3 v1 = dot(-vel2,N)*N;
 			vect3 v2 = dot(-vel1,N)*N;
@@ -231,11 +240,23 @@ void Lab9::Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, floa
 		b2.mat = mrotateByAxis( b2.vaxis, b2.fspin ) * b2.mat;
 	}
 	
+	m.drawVect( gra, pers, text_y, b1.pos, b1.vaxis.normalize() ,1	, col3, "axis" );
 	m.drawVect( gra, pers, text_y, b2.pos, b2.vaxis.normalize() ,1	, col3, "axis" );
 
+
+	// 平面表示
+	{
+		pers.prim.DrawPlate( gra, pers, plate_p, plate_n, 28, rgb(0.5,1,1)*0.55 );
+	}
+	
 	
 	// 表示
 	pers.prim.DrawSphere( gra, pers, b1.radius, b1.pos, b1.mat );
 	pers.prim.DrawSphere( gra, pers, b2.radius, b2.pos, b2.mat );
+
+	// 接点表示
+	if ( b1.flgOn )	pers.pen.pset3d( gra, pers, b1.Q , col6, 9);
+	if ( b2.flgOn )	pers.pen.pset3d( gra, pers, b2.Q , col6, 9);
+
 
 }
