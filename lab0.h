476eb53a54ca6fabc0db58a9a11d3d6ba7d1e0ac
nishoikozo <nishiokozo@gmail.com>
2019-12-09 12:06:27 +0900

    lab0 update

--- lab0.h
+++ lab0.h
@@ -1,8 +1,4 @@
 struct Lab0 : LabObj
 {
-	Lab0();
 	void Update( SysKeys& keys, SysMouse& mouse, SysGra& gra, Pers& pers, float delta, int& text_y, Cp& cp );
-
-    struct Impl;
-    std::unique_ptr<Impl> pImpl;
 };
