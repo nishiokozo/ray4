f055e2cca8f34ad6178d1bc90abf4544b413ff8e
nishoikozo <nishiokozo@gmail.com>
2019-12-26 16:13:10 +0900

    lab27 N1基底関数をパラメータ化

--- lab27.cpp
+++ lab27.cpp
@@ -75,26 +75,13 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	function<double(double,double,double)> N1 = []( double t, double lo, double hi )
 	{
 		if ( t < lo || t > hi ) return 0.0;
-		return ( t < lo+1.0 )?t:hi-t; 
+		return ( t < lo+1.0 )?t-lo:hi-t; 
 	};
 
-	// N1a(t) 一次基底関数 山型波形 0.0~2.0 
-	function<double(double)> N1a = []( double t )
-	{
-		if ( t < 0.0 || t > 2.0 ) return 0.0;
-		return ( t < 1.0 )?t:2.0-t; 
-	};
 	// f1a(t) 右肩上がり
 	function<double(double)> f1a = []( double t )
 	{
 		return t/2.0; 
-	};
-
-	// N1b(t) 一次基底関数 山型波形 1.0~3.0
-	function<double(double)> N1b = []( double t )
-	{
-		if ( t < 1.0 || t > 3.0 ) return 0.0;
-		return ( t < 2.0 )?t-1.0:3.0-t; 
 	};
 	// f1b(t) 右肩下がり
 	function<double(double)> f1b = []( double t )
@@ -105,11 +92,11 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	// N2(t) 二次基底関数 
 	function<double(double)> N2 = [&]( double t )
 	{
-		double a1 = N1a(t);
+		double a1 = N1(t,0,2);
 		double b1 = f1a(t);
 		double y1 = a1 * b1;
 
-		double a2 = N1b(t);
+		double a2 = N1(t,1,3);
 		double b2 = f1b(t);
 		double y2 = a2 * b2;
 
@@ -131,14 +118,14 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	{
 
 		// 左2/3
-		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1a(t), 0), rgb(1,1,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1(t,0,2), 0), rgb(1,1,0), 1 );
 		pers.pen.pset3d( gra, pers, vect3(t, 4+ f1a(t), 0), rgb(1,0,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1a(t)*f1a(t), 0), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1(t,0,2)*f1a(t), 0), rgb(1,1,1), 2 );
 
 		// 右2/3
-		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1b(t), 0), rgb(1,1,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1(t,1,3), 0), rgb(1,1,0), 1 );
 		pers.pen.pset3d( gra, pers, vect3(t, 2+ f1b(t), 0), rgb(1,0,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1b(t)*f1b(t), 0 ), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1(t,1,3)*f1b(t), 0 ), rgb(1,1,1), 2 );
 
 		// 左右合成
 		pers.pen.pset3d( gra, pers, vect3(t, N2(t) ,0), rgb(1,1,1), 2 );
