2f1cac8bf32df86c99767e422cf0c41b8dab239c
nishoikozo <nishiokozo@gmail.com>
2019-12-26 16:09:52 +0900

    lab27 修正

--- lab27.cpp
+++ lab27.cpp
@@ -52,7 +52,7 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	// 画面クリア
 	gra.Clr(rgb(0.3,0.3,0.3));
 	pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), mrotx(rad(90)), 10, 10, 1, rgb(0.2,0.2,0.2) );
-	gra.Print(1,(float)text_y++,"27 : 2D B-Spline"); 
+	gra.Print(1,(double)text_y++,"27 : 2D B-Spline"); 
 
 	//初期化
 	if ( pImpl->bResetAll )
@@ -71,56 +71,63 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	}
 	
 	
+	// N1(t) 一次基底関数 山型波形
+	function<double(double,double,double)> N1 = []( double t, double lo, double hi )
+	{
+		if ( t < lo || t > hi ) return 0.0;
+		return ( t < lo+1.0 )?t:hi-t; 
+	};
+
 	// N1a(t) 一次基底関数 山型波形 0.0~2.0 
-	function<float(float)> N1a = []( float t )
+	function<double(double)> N1a = []( double t )
 	{
 		if ( t < 0.0 || t > 2.0 ) return 0.0;
 		return ( t < 1.0 )?t:2.0-t; 
 	};
 	// f1a(t) 右肩上がり
-	function<float(float)> f1a = []( float t )
+	function<double(double)> f1a = []( double t )
 	{
 		return t/2.0; 
 	};
 
 	// N1b(t) 一次基底関数 山型波形 1.0~3.0
-	function<float(float)> N1b = []( float t )
+	function<double(double)> N1b = []( double t )
 	{
 		if ( t < 1.0 || t > 3.0 ) return 0.0;
 		return ( t < 2.0 )?t-1.0:3.0-t; 
 	};
 	// f1b(t) 右肩下がり
-	function<float(float)> f1b = []( float t )
+	function<double(double)> f1b = []( double t )
 	{
 		return (3.0-t)/2.0; 
 	};
 
 	// N2(t) 二次基底関数 
-	function<float(float)> N2 = [&]( float t )
+	function<double(double)> N2 = [&]( double t )
 	{
-		float a1 = N1a(t);
-		float b1 = f1a(t);
-		float y1 = a1 * b1;
+		double a1 = N1a(t);
+		double b1 = f1a(t);
+		double y1 = a1 * b1;
 
-		float a2 = N1b(t);
-		float b2 = f1b(t);
-		float y2 = a2 * b2;
+		double a2 = N1b(t);
+		double b2 = f1b(t);
+		double y2 = a2 * b2;
 
-		float y3 = y1 + y2;
+		double y3 = y1 + y2;
 		return y3; 
 	};
 	// f2a(t) 右肩上がり
-	function<float(float)> f2a = []( float t )
+	function<double(double)> f2a = []( double t )
 	{
 		return t/3.0; 
 	};
 	// f2b(t) 右肩下がり
-	function<float(float)> f2b = []( float t )
+	function<double(double)> f2b = []( double t )
 	{
 		return (4.0-t)/3.0; 
 	};
 
-	for ( float t = 0 ; t <= 3.0  ; t+=0.01 )
+	for ( double t = 0 ; t <= 3.0  ; t+=0.01 )
 	{
 
 		// 左2/3
@@ -131,7 +138,7 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 		// 右2/3
 		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1b(t), 0), rgb(1,1,0), 1 );
 		pers.pen.pset3d( gra, pers, vect3(t, 2+ f1b(t), 0), rgb(1,0,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1a(t)*f1b(t), 0 ), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1b(t)*f1b(t), 0 ), rgb(1,1,1), 2 );
 
 		// 左右合成
 		pers.pen.pset3d( gra, pers, vect3(t, N2(t) ,0), rgb(1,1,1), 2 );
