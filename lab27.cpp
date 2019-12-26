bcb657174639efc82bd47c281bd26adefdbbfa46
nishoikozo <nishiokozo@gmail.com>
2019-12-26 16:04:09 +0900

    lab27 説明追加

--- lab27.cpp
+++ lab27.cpp
@@ -60,8 +60,8 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 		pImpl->bResetAll = false;
 
 		// カメラ
-		pers.cam.pos = vect3( 1.0, 2.0, -6.0 );
-		pers.cam.at = vect3( 1.0,  2.0, 0 );
+		pers.cam.pos = vect3( 1.0, 3.0, -9.0 );
+		pers.cam.at = vect3( 1.0,  3.0, 0 );
 		pers.fovy=rad(45);
 	}
 
@@ -71,53 +71,70 @@ void Lab27::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 	}
 	
 	
-	// 山型波形 0.0~2.0
-	function<float(float)> N0_1 = []( float t )
+	// N1a(t) 一次基底関数 山型波形 0.0~2.0 
+	function<float(float)> N1a = []( float t )
 	{
 		if ( t < 0.0 || t > 2.0 ) return 0.0;
 		return ( t < 1.0 )?t:2.0-t; 
 	};
-	function<float(float)> f1 = []( float t )
+	// f1a(t) 右肩上がり
+	function<float(float)> f1a = []( float t )
 	{
-		return 0.5f * t; 
+		return t/2.0; 
 	};
 
-	// 山型波形 1.0~3.0
-	function<float(float)> N1_1 = []( float t )
+	// N1b(t) 一次基底関数 山型波形 1.0~3.0
+	function<float(float)> N1b = []( float t )
 	{
 		if ( t < 1.0 || t > 3.0 ) return 0.0;
 		return ( t < 2.0 )?t-1.0:3.0-t; 
 	};
-	function<float(float)> f2 = []( float t )
+	// f1b(t) 右肩下がり
+	function<float(float)> f1b = []( float t )
 	{
 		return (3.0-t)/2.0; 
+	};
+
+	// N2(t) 二次基底関数 
+	function<float(float)> N2 = [&]( float t )
+	{
+		float a1 = N1a(t);
+		float b1 = f1a(t);
+		float y1 = a1 * b1;
+
+		float a2 = N1b(t);
+		float b2 = f1b(t);
+		float y2 = a2 * b2;
+
+		float y3 = y1 + y2;
+		return y3; 
+	};
+	// f2a(t) 右肩上がり
+	function<float(float)> f2a = []( float t )
+	{
+		return t/3.0; 
+	};
+	// f2b(t) 右肩下がり
+	function<float(float)> f2b = []( float t )
+	{
+		return (4.0-t)/3.0; 
 	};
 
 	for ( float t = 0 ; t <= 3.0  ; t+=0.01 )
 	{
 
-		float a1 = N0_1(t);
-		float b1 = f1(t);
-		float y1 = a1 * b1;
-
-		float a2 = N1_1(t);
-		float b2 = f2(t);
-		float y2 = a2 * b2;
-
-		float y3 = y1 + y2;
-
 		// 左2/3
-		pers.pen.pset3d( gra, pers, vect3(t, 4+a1,0), rgb(1,1,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 4+b1,0), rgb(1,0,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 4+y1,0), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1a(t), 0), rgb(1,1,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 4+ f1a(t), 0), rgb(1,0,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 4+ N1a(t)*f1a(t), 0), rgb(1,1,1), 2 );
 
 		// 右2/3
-		pers.pen.pset3d( gra, pers, vect3(t, 2+a2,0), rgb(1,1,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 2+b2,0), rgb(1,0,0), 1 );
-		pers.pen.pset3d( gra, pers, vect3(t, 2+y2,0), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1b(t), 0), rgb(1,1,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ f1b(t), 0), rgb(1,0,0), 1 );
+		pers.pen.pset3d( gra, pers, vect3(t, 2+ N1a(t)*f1b(t), 0 ), rgb(1,1,1), 2 );
 
 		// 左右合成
-		pers.pen.pset3d( gra, pers, vect3(t, y3 ,0), rgb(1,1,1), 2 );
+		pers.pen.pset3d( gra, pers, vect3(t, N2(t) ,0), rgb(1,1,1), 2 );
 	}
 	
 	
