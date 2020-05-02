7cf2e0f364ef96c96b27cc59111a0a9072bd2e29
nishiokozo <nishiokozo@gmail.com>
2020-05-02 18:51:47 +0900

    LAB35 タイトルシーケンスの追加

--- lab35.cpp
+++ lab35.cpp
@@ -32,10 +32,25 @@ using namespace std;
 
 struct Lab35::Impl
 {
-	bool	bResetAll = true;
-	bool	bResetParam = true;
-	bool	bPause = false;
-	bool	bStep = false;
+	const int	RINGSIZE =	10;
+
+//	bool	bResetAll = true;
+//	bool	bResetParam = true;
+//	bool	bPause = false;
+//	bool	bStep = false;
+
+
+	enum State
+	{
+		Reset,
+		Title,
+		Play,
+		End,
+	};
+
+
+	State	valState = State::Reset;
+	float	timeState = 0.0;
 
 //	vector<shared_ptr<Obj>>	tbl_pObj;
 	struct Stone
@@ -49,10 +64,169 @@ struct Lab35::Impl
 		Stone( vect2 _pos, float _r, vect2 _mov ) :  pos(_pos), radius(_r), vel(_mov) {}
 	};
 
-	vector<Stone>	tblStones;
+	vector<Stone>	tblStone;
 	
 //	mat33	mat = mat33::mrotx(rad(90));
 	mat33	mat = mat33::mrotx(rad(0));
+
+	//------------------------------------------------------------------------------
+	void StateReset( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra, Pers& pers )
+	//------------------------------------------------------------------------------
+	{
+
+		// カメラ
+		pers.cam.pos = vect3( 0.0, 0.0, -25.0 );
+		pers.cam.at = vect3( 0,  0.0, 0 );
+		pers.cam.Update();
+
+		// ストーン
+//		pImpl->tblStone.emplace_back( Impl::Stone(vect2(1,0),0.5) );
+//		pImpl->tblStone.emplace_back( Impl::Stone(vect2(0,1),0.5) );
+		this->tblStone.clear();
+		float step = 5;
+		for ( float x = -RINGSIZE+step/2 ; x < RINGSIZE-1 ; x += step )
+		{
+			for ( float y = -RINGSIZE+step/2 ; y < RINGSIZE-1 ; y += step )
+			{
+				this->tblStone.emplace_back( Stone(vect2(x,y),0.5) );
+			}
+		}
+		this->valState = State::Title;
+		this->timeState = 0.0;
+	}
+
+	//------------------------------------------------------------------------------
+	void StateTitle( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra, Pers& pers )
+	//------------------------------------------------------------------------------
+	{
+		// 画面クリア
+//		gra.Clr(rgb(0.3,0.3,0.3));
+//		pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), pImpl->mat, pImpl->RINGSIZE,pImpl->RINGSIZE, 1, rgb(0.2,0.2,0.2) );
+//		gra.Print(1,(float)text_y++,"35 : Curing(Title)" ); 
+
+		gra.Print(16,10," Curing Game" ); 
+
+		
+		gra.Print(16,20," Push Mouse.L Button " ); 
+		
+		if ( mouse.L.hi ) this->valState = State::Play;
+	}
+	
+	//------------------------------------------------------------------------------
+	void StatePlay( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra, Pers& pers )
+	//------------------------------------------------------------------------------
+	{
+		// 画面クリア
+		pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), this->mat, this->RINGSIZE,this->RINGSIZE, 1, rgb(0.2,0.2,0.2) );
+
+		{
+			vect3 P1 = pers.calcScreenToWorld3( vect3(mouse.pos,0) );
+			vect3 I1 = pers.calcRayvect( P1 );
+
+			auto[b,Q] = pers.grid.IntersectOn( P1, I1 );	// Grid空間座標を求める。
+
+			pers.grid.Circle( gra, pers,Q, 0.5, 24, rgb(0,1,0) );
+			pers.grid.Pset( gra, pers, Q, rgb(0,1,0), 12 );
+			pers.grid.Print( gra, pers, Q, -26,-52, to_string(Q.x) + " " +to_string(Q.y)  ); 
+	//			pers.grid.Line( gra, pers, vect2(1,1), vect2(-1,-1), rgb(0,1,1), 2 );
+
+
+			if ( timeState > 1.0 )
+			{
+
+				if ( mouse.L.lo ) this->tblStone.emplace_back( Q, 0.5, vect2(0,0.1) );
+				if ( mouse.F.lo ) this->tblStone.emplace_back( Q, 0.5, vect2(0,0.4) );
+				if ( mouse.B.lo ) this->tblStone.emplace_back( Q, 0.5, vect2(0,0.8) );
+			}
+
+		}
+
+
+		// 移動
+		for ( auto& o : this->tblStone )
+		{
+			o.prev_pos = o.pos;
+			o.pos += o.vel;
+		//	o.vel *= 0.9999;
+		}
+
+		// 衝突：壁
+		for ( auto& b1 : this->tblStone )
+		{
+			if ( b1.pos.y + b1.radius > RINGSIZE )
+			{
+				b1.pos = b1.prev_pos;
+				b1.vel.y = -b1.vel.y;
+			}
+			if ( b1.pos.y - b1.radius < -RINGSIZE )
+			{
+				b1.pos = b1.prev_pos;
+				b1.vel.y = -b1.vel.y;
+			}
+
+			if ( b1.pos.x + b1.radius > RINGSIZE )
+			{
+				b1.pos = b1.prev_pos;
+				b1.vel.x = -b1.vel.x;
+			}
+			if ( b1.pos.x - b1.radius < -RINGSIZE )
+			{
+				b1.pos = b1.prev_pos;
+				b1.vel.x = -b1.vel.x;
+			}
+		//		pers.grid.Print( gra, pers, b1.pos, -26,-52, to_string(b1.pos.x) + " " +to_string(b1.pos.y)  ); 
+		}
+		
+		// 衝突：ストーン同士
+		for ( auto& b1 : this->tblStone )
+		{
+			for ( auto& b2 : this->tblStone )
+			{
+				if ( &b1 != &b2 )
+				{
+					vect2	v = (b2.pos-b1.pos);
+					float	len = v.abs();
+					float	bor = b1.radius + b2.radius;
+					if ( len < bor )
+					{
+						b1.pos = b1.prev_pos;
+						b2.pos = b2.prev_pos;
+
+						v = v * (bor-len);
+						vect2	N = v.normalize();
+						vect2	vel1 = func_reflect( b1.vel, N, 1.0 );
+						vect2	vel2 = func_reflect( b2.vel, N, 1.0 );
+
+						vect2	v1 = dot(-vel2,N)*N;
+						vect2	v2 = dot(-vel1,N)*N;
+
+						b1.vel += v1 - v2;
+						b2.vel += v2 - v1;
+
+					}
+				}
+			}
+		}
+
+		// 減衰
+		for ( auto& o : this->tblStone )
+		{
+			o.vel *= 0.999;
+		}
+
+		// 表示
+		for ( auto o : this->tblStone )
+		{
+			pers.grid.Circle( gra, pers,o.pos, o.radius, 24, rgb(1,1,1) );
+			pers.grid.Line( gra, pers, vect2(-RINGSIZE,-RINGSIZE), vect2(-RINGSIZE, RINGSIZE), rgb(1,1,1), 2 );
+			pers.grid.Line( gra, pers, vect2( RINGSIZE,-RINGSIZE), vect2( RINGSIZE, RINGSIZE), rgb(1,1,1), 2 );
+			pers.grid.Line( gra, pers, vect2(-RINGSIZE,-RINGSIZE), vect2( RINGSIZE,-RINGSIZE), rgb(1,1,1), 2 );
+			pers.grid.Line( gra, pers, vect2(-RINGSIZE, RINGSIZE), vect2( RINGSIZE, RINGSIZE), rgb(1,1,1), 2 );
+
+		}
+	}
+
+
 };
 Lab35::Lab35() : pImpl( new Lab35::Impl ){}
 
@@ -60,26 +234,45 @@ Lab35::Lab35() : pImpl( new Lab35::Impl ){}
 void Lab35::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra, Pers& pers, float delta, int& text_y, Cp& cp )
 //------------------------------------------------------------------------------
 {
-	// 画面クリア
-	gra.Clr(rgb(0.3,0.3,0.3));
-	pers.grid.DrawGrid3d( gra, pers, vect3(0,0,0), pImpl->mat, 10, 10, 1, rgb(0.2,0.2,0.2) );
-	gra.Print(1,(float)text_y++,"35 : Curing" ); 
+		gra.Clr(rgb(0.3,0.3,0.3));
+		gra.Print(1,(float)text_y++,"35 : Curing " + to_string(pImpl->timeState) ); 
+
+
 
 	//初期化
-	if ( pImpl->bResetAll )
+//	if ( pImpl->bResetAll )
+//	{
+//		pImpl->bResetAll = false;
+//	}
+
+	Impl::State prev = pImpl->valState;
+
+	switch( pImpl->valState )
 	{
-		pImpl->bResetAll = false;
+		case Impl::State::Reset:
+			pImpl->StateReset( keys, mouse, sound, gra, pers );
+		
+			break;
+		case Impl::State::Title:
+			pImpl->StateTitle( keys, mouse, sound, gra, pers );
+			break;
 
-		// カメラ
-		pers.cam.pos = vect3( 0.0, 0.0, -15.0 );
-		pers.cam.at = vect3( 0,  0.0, 0 );
-		pers.cam.Update();
-
-		// ストーン
-		pImpl->tblStones.emplace_back( Impl::Stone(vect2(1,0),0.5) );
-		pImpl->tblStones.emplace_back( Impl::Stone(vect2(0,1),0.5) );
-
+		case Impl::State::Play:
+			pImpl->StatePlay( keys, mouse, sound, gra, pers );
+			break;
+		default:
+			break;
 	}
+
+	if ( prev != pImpl->valState )
+	{
+		pImpl->timeState = 0;
+	}
+	else
+	{
+		pImpl->timeState += delta;
+	}
+
 /*
 	vect3 vx = vect3(1,0,0);
 	vect3 vy = vect3(0,1,0);
@@ -102,78 +295,13 @@ void Lab35::Update( SysKeys& keys, SysMouse& mouse, SysSound& sound, SysGra& gra
 
 	}
 */
-	if ( keys.R.hi )	pImpl->bResetAll = true;
+	if ( keys.R.hi )	pImpl->valState = Impl::State::Reset;
 	if ( keys.Q.hi )	pImpl->mat *= mat33::mrotx(rad(-30));
 	if ( keys.A.hi )	pImpl->mat *= mat33::mrotx(rad(30));
 
-	{
-		vect3 P1 = pers.calcScreenToWorld3( vect3(mouse.pos,0) );
-		vect3 I1 = pers.calcRayvect( P1 );
 
-		auto[b,Q] = pers.grid.IntersectOn( P1, I1 );	// Grid空間座標を求める。
-
-		pers.grid.Circle( gra, pers,Q, 0.5, 24, rgb(0,1,0) );
-		pers.grid.Pset( gra, pers, Q, rgb(0,1,0), 12 );
-		pers.grid.Print( gra, pers, Q, -26,-52, to_string(Q.x) + " " +to_string(Q.y)  ); 
-//			pers.grid.Line( gra, pers, vect2(1,1), vect2(-1,-1), rgb(0,1,1), 2 );
-
-
-		if ( mouse.L.lo ) pImpl->tblStones.emplace_back( Q, 0.5, vect2(0,0.01) );
-
-	}
-
-
-	// 移動
-	for ( auto& o : pImpl->tblStones )
-	{
-		o.prev_pos = o.pos;
-		o.pos += o.vel;
-		o.vel *= 0.9999;
-	}
-
-	// 衝突
-	for ( auto& b1 : pImpl->tblStones )
-	{
-		for ( auto& b2 : pImpl->tblStones )
-		{
-			if ( &b1 != &b2 )
-			{
-				vect2	v = (b2.pos-b1.pos);
-				float	len = v.abs();
-				float	bor = b1.radius + b2.radius;
-				if ( len < bor )
-				{
-					b1.pos = b1.prev_pos;
-					b2.pos = b2.prev_pos;
-
-					v = v * (bor-len);
-					vect2	N = v.normalize();
-					vect2	vel1 = func_reflect( b1.vel, N, 1.0 );
-					vect2	vel2 = func_reflect( b2.vel, N, 1.0 );
-
-					vect2	v1 = dot(-vel2,N)*N;
-					vect2	v2 = dot(-vel1,N)*N;
-
-					b1.vel += v1 - v2;
-					b2.vel += v2 - v1;
-
-				}
-			}
-		}
-	}
-
-	// 減衰
-	for ( auto& o : pImpl->tblStones )
-	{
-		o.vel *= 0.999;
-	}
-
-	// 表示
-	for ( auto o : pImpl->tblStones )
-	{
-		pers.grid.Circle( gra, pers,o.pos, o.radius, 24, rgb(1,1,1) );
-	}
 //	pers.prim.DrawCircle( gra, pers, vect3( 0,0, 0 ), mat33::mrotz(0.0), 1.0, rgb(1,1,1) );
 
 
+	gra.Wait(); 
 }
